9863aeaac7082e731d632fc951edfcc5b2113d73
Silence <374619540@qq.com>
2017-04-01 12:51:48 +0800

    C语言混编

---
+++ Learn/20.混编/C语言基本数据类型.playground/Contents.swift
@@ -0,0 +1,14 @@
+
+
+import Foundation
+
+var intSwift = 80
+
+// int
+var intNumber  = NSNumber(value: CInt(intSwift))
+
+// unsigned char
+var unsignedCharNumber  = NSNumber(value: CUnsignedChar(intSwift))
+
+// unsigned int
+var unsignedIntNumber  = NSNumber(value: CUnsignedInt(intSwift))
