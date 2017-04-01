88ae2196f906a8cd8e93c2f335fb637a77f2a9db
Silence <374619540@qq.com>
2017-04-01 22:42:16 +0800

    ObjcDynamic

---
+++ Swifter/33ObjcDynamic.playground/Contents.swift
@@ -0,0 +1,13 @@
+//: Playground - noun: a place where people can play
+
+import Foundation
+
+@objc(MyClass)
+class 我的类: NSObject {
+    @objc(greeting:)
+    func 打招呼(名字: String) {
+        print("哈喽，\(名字)")
+    }
+}
+
+我的类().打招呼(名字: "小明")
