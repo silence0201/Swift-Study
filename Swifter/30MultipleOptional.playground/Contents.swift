acdf9b86d199b5213ab2a0aa15ccea58409a3663
Silence <374619540@qq.com>
2017-04-01 22:03:46 +0800

    多重Optional

---
+++ Swifter/30MultipleOptional.playground/Contents.swift
@@ -0,0 +1,24 @@
+//: Playground - noun: a place where people can play
+
+import Foundation
+
+var string: String? = "string"
+var anotherString: String?? = string
+
+var literalOptional: String?? = "string"
+
+var aNil: String? = nil
+
+var anotherNil: String?? = aNil
+var literalNil: String?? = nil
+
+if let a = anotherNil {
+    // This will output.
+    print("anotherNil")
+}
+
+if let b = literalNil {
+    // This will not output.
+    print("literalNil")
+}
+
