036441251e5570fad3b98ca0510c0592c0f85045
Silence <374619540@qq.com>
2017-04-01 19:12:32 +0800

    DefaultParam

---
+++ Swifter/20DefaultParam.playground/Contents.swift
@@ -0,0 +1,15 @@
+//: Playground - noun: a place where people can play
+
+
+import Foundation
+
+func sayHello1(str1: String = "Hello", str2: String, str3: String) {
+    print(str1 + str2 + str3)
+}
+
+func sayHello2(str1: String, str2: String, str3: String = "World") {
+    print(str1 + str2 + str3)
+}
+
+sayHello1(str2: " ", str3: "World")
+sayHello2(str1: "Hello", str2: " ")
