4740b30b85b3530cdcabe6d7f87bd21640c659be
Silence <374619540@qq.com>
2017-04-01 22:05:17 +0800

    Optional Map

---
+++ Swifter/31OptionalMap.playground/Contents.swift
@@ -0,0 +1,20 @@
+//: Playground - noun: a place where people can play
+
+import Foundation
+
+let arr = [1,2,3]
+let doubled = arr.map{
+    $0 * 2
+}
+
+print(doubled)
+// 输出：
+// [2,4,6]
+
+let num: Int? = 3
+let result = num.map {
+    $0 * 2
+}
+
+result
+// result 为 {Some 6}
