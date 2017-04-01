500251050f6b2cd727534881bb9796fe2949df8f
Silence <374619540@qq.com>
2017-04-01 16:43:18 +0800

    可变参数

---
+++ Swifter/14Variadic.playground/Contents.swift
@@ -0,0 +1,23 @@
+//: Playground - noun: a place where people can play
+
+import Foundation
+
+func sum(input: Int...) -> Int {
+    return input.reduce(0, +)
+}
+
+print(sum(input: 1,2,3,4,5))
+
+func myFunc(numbers: Int..., string: String) {
+    numbers.forEach {
+        for i in 0..<$0 {
+            print("\(i + 1): \(string)")
+        }
+    }
+}
+
+myFunc(numbers: 1, 2, 3, string: "hello")
+
+let name = "Tom"
+let date = NSDate()
+let string = NSString(format: "Hello %@. Date: %@", name, date)
