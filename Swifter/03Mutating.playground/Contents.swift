c775491cc0e4c23d573c95248381f602ff759380
Silence <374619540@qq.com>
2017-04-01 14:28:01 +0800

    Mutating

---
+++ Swifter/03Mutating.playground/Contents.swift
@@ -0,0 +1,30 @@
+//: Playground - noun: a place where people can play
+
+import Foundation
+
+/* Wrong code
+ struct User {
+ var age : Int
+ var weight : Int
+ var height : Int
+ 
+ func gainWeight(newWeight: Int) {
+ weight += newWeight
+ }
+ }
+ */
+
+struct User {
+    var age: Int
+    var weight: Int
+    var height: Int
+    
+    mutating func gainWeight(newWeight: Int) {
+        weight += newWeight
+    }
+}
+
+var u = User(age: 1, weight: 1, height: 1)
+
+u.gainWeight(newWeight: 2)
+u
