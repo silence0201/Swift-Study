65554b2cdb10e127ef569512ca40b8965a3f5ec1
Silence <374619540@qq.com>
2017-04-02 10:38:22 +0800

    delegate

---
+++ Swifter/48Delegate.playground/Contents.swift
@@ -0,0 +1,32 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+protocol MyClassDelegate: class {
+    func method()
+}
+
+class MyClass {
+    weak var delegate: MyClassDelegate?
+}
+
+class ViewController: UIViewController, MyClassDelegate {
+    // ...
+    var someInstance: MyClass!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        someInstance = MyClass()
+        someInstance.delegate = self
+    }
+    
+    func method() {
+        print("Do something")
+    }
+    
+    //...
+}
+
+// weak var delegate: MyClassDelegate? 编译错误
+// 'weak' cannot be applied to non-class type 'MyClassDelegate'
