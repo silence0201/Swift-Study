370d4cd5e9da04dfb3929c5099a4d464bd2b561b
Silence <374619540@qq.com>
2017-04-02 09:50:58 +0800

    equal

---
+++ Swifter/41Equal.playground/Contents.swift
@@ -0,0 +1,29 @@
+//: Playground - noun: a place where people can play
+
+
+import Foundation
+
+let str1 = "快乐的字符串"
+let str2 = "快乐的字符串"
+let str3 = "开心的字符串"
+
+str1 == str2  // true
+str1 == str3  // false
+
+class TodoItem {
+    let uuid: String
+    var title: String
+    
+    init(uuid: String, title: String) {
+        self.uuid = uuid
+        self.title = title
+    }
+}
+
+extension TodoItem: Equatable {
+    
+}
+
+func ==(lhs: TodoItem, rhs: TodoItem) -> Bool {
+    return lhs.uuid == rhs.uuid
+}
