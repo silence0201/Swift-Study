d60f8f2b1fed4ee38bf4e83f4175ec4d9bd93909
Silence <374619540@qq.com>
2017-04-02 10:52:37 +0800

    Lock

---
+++ Swifter/50Lock.playground/Contents.swift
@@ -0,0 +1,24 @@
+//: Playground - noun: a place where people can play
+
+import Foundation
+
+func myMethod(anObj: AnyObject!) {
+    objc_sync_enter(anObj)
+    
+    // 在 enter 和 exit 之间 anObj 不会被其他线程改变
+    
+    objc_sync_exit(anObj)
+}
+
+func synchronized(_ lock: AnyObject, closure: () -> ()) {
+    objc_sync_enter(lock)
+    closure()
+    objc_sync_exit(lock)
+}
+
+func myMethodLocked(anObj: AnyObject!) {
+    synchronized(anObj) {
+        // 在括号内 anObj 不会被其他线程改变
+    }
+}
+
