8934a89af430c0822cf223e23ae813faa2e24739
Silence <374619540@qq.com>
2017-04-02 09:41:51 +0800

    KVO

---
+++ Swifter/39KVO.playground/Contents.swift
@@ -0,0 +1,52 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+import PlaygroundSupport
+
+PlaygroundPage.current.needsIndefiniteExecution = true
+
+class MyClass: NSObject {
+    dynamic var date = Date()
+}
+
+class MyClass1: NSObject {
+    var date = Date()
+}
+
+class MyChildClass: MyClass1 {
+    dynamic override var date: Date {
+        get { return super.date }
+        set { super.date = newValue }
+    }
+}
+
+private var myContext = 0
+
+class Class: NSObject {
+    
+    var myObject: MyClass!
+    
+    override init() {
+        super.init()
+        myObject = MyClass()
+        print("初始化 MyClass，当前日期: \(myObject.date)")
+        myObject.addObserver(self,
+                             forKeyPath: "date",
+                             options: .new,
+                             context: &myContext)
+    }
+    
+    override func observeValue(forKeyPath keyPath: String?,
+                               of object: Any?,
+                               change: [NSKeyValueChangeKey : Any]?,
+                               context: UnsafeMutableRawPointer?)
+    {
+        if let change = change, context == &myContext {
+            let newDate = change[.newKey]
+            print("日期发生变化 \(newDate)")
+        }
+    }
+}
+
+let obj = Class()
+
