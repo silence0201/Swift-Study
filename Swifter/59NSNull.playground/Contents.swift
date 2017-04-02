3a40a36570c1c46818253f870b36242842915cbb
Silence <374619540@qq.com>
2017-04-02 13:01:01 +0800

    NSNull

---
+++ Swifter/59NSNull.playground/Contents.swift
@@ -0,0 +1,15 @@
+//: Playground - noun: a place where people can play
+
+import Foundation
+
+// 假设 jsonValue 是从一个 JSON 中取出的 NSNull
+let jsonValue: AnyObject = NSNull()
+
+if let string = jsonValue as? String {
+    print(string.hasPrefix("a"))
+} else {
+    print("不能解析")
+}
+
+// 输出：
+// 不能解析
