c7a7c03a4d1729aa00b0582920a2e9ad6cca782b
Silence <374619540@qq.com>
2017-04-02 13:10:29 +0800

    documentation

---
+++ Swifter/61Documentation.playground/Contents.swift
@@ -0,0 +1,19 @@
+//: Playground - noun: a place where people can play
+
+import Foundation
+
+/**
+ A demo method
+ 
+ - parameter input: An Int number
+ 
+ - returns: The string represents the input number
+ */
+func method(input: Int) -> String {
+    return String(input)
+}
+
+struct Person {
+    /// name of the person
+    var name: String
+}
